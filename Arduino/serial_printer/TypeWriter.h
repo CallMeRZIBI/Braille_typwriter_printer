80f0216305baeb4d38e3c750c4268d55d010f00c
CallMeRZIBI <michalenko.jakub@gmail.com>
2022-10-05 22:23:37 +0200

    extended the alphabet

--- Arduino/serial_printer/TypeWriter.h
+++ Arduino/serial_printer/TypeWriter.h
@@ -18,7 +18,7 @@ class TypeWriter{
     double _degrees = 30;
     int _brailleDotsCount = 7;
     int _stepNumber = 0;
-    static const int _brailleDLength = 37;
+    static const int _brailleDLength = 70;
 
     void newLine();
     void OneStep(bool dir);
@@ -35,6 +35,7 @@ class TypeWriter{
 
     // I really don't know why was this the problem (probably because this is a header file)
     // but I had to add size to _brailledict to compile
+    // This is now Czech braille alphabet
     const _brailleChar _brailleDict[_brailleDLength] = {{'a', new int[1]{1}, 1},
                                        {'b', new int[2]{1,2}, 2},
                                        {'c', new int[2]{1,4}, 2},
@@ -59,10 +60,26 @@ class TypeWriter{
                                        {'v', new int[4]{1,2,3,6}, 4},
                                        {'w', new int[4]{2,4,5,6}, 4},
                                        {'x', new int[4]{1,3,4,6}, 4},
-                                       {'y', new int[5]{1,3,4,5,6}, 5},
+                                       //{'y', new int[5]{1,3,4,5,6}, 5},  // Czech one is different
                                        {'z', new int[4]{1,3,5,6}, 4},
-                                       {' ', new int[1]{0}, 1},
-                                       {'1', new int[1]{1}, 1},
+                                       {' ', new int[1]{0}, 1},  // End of standard
+                                       {'á', new int[2]{1,6}, 2},
+                                       {'č', new int[3]{1,4,6}, 3},
+                                       {'ď', new int[4]{1,4,5,6}, 4},
+                                       {'é', new int[3]{3,4,5}, 3},
+                                       {'ě', new int[3]{1,2,6}, 3},
+                                       {'í', new int[2]{3,4}, 2},
+                                       {'ň', new int[4]{1,2,4,6}, 4},
+                                       {'ó', new int[3]{2,4,6}, 3},
+                                       {'ř', new int[4]{2,4,5,6}, 4},
+                                       {'š', new int[3]{1,5,6}, 3},
+                                       {'ť', new int[4]{1,2,5,6}, 4},
+                                       {'ů', new int[3]{3,4,6}, 3},
+                                       {'ú', new int[5]{2,3,4,5,6}, 5},
+                                       {'y', new int[5]{1,2,3,5,6}, 5},
+                                       {'ý', new int[5]{1,2,3,4,6}, 5},
+                                       {'ž', new int[4]{2,3,4,6}, 4},  // End of Czech extend
+                                       {'1', new int[1]{1}, 1},  // Numbers
                                        {'2', new int[2]{1,2}, 2},
                                        {'3', new int[2]{1,4}, 2},
                                        {'4', new int[3]{1,4,5}, 3},
@@ -71,7 +88,25 @@ class TypeWriter{
                                        {'7', new int[4]{1,2,4,5}, 4},
                                        {'8', new int[3]{1,2,5}, 3},
                                        {'9', new int[2]{2,4}, 2},
-                                       {'0', new int[3]{2,4,5}, 3}
+                                       {'0', new int[3]{2,4,5}, 3}, // End of numbers
+                                       {'.', new int[1]{3}, 1},  // Special characters
+                                       {',', new int[1]{2}, 1},
+                                       {'!', new int[3]{2,3,5}, 3},
+                                       {'?', new int[2]{2,6}, 2},
+                                       {':', new int[2]{2,4}, 2},
+                                       {';', new int[2]{2,3}, 2},
+                                       {'(', new int[3]{2,3,6}, 3},
+                                       {')', new int[3]{3,5,6}, 3},
+                                       {'+', new int[3]{2,5,6}, 3},
+                                       {'-', new int[2]{3,6}, 2},
+                                       {'=', new int[4]{2,3,5,6}, 4},
+                                       {'<', new int[3]{1,2,6}, 3},
+                                       {'>', new int[3]{3,4,5}, 3},
+                                       {'"', new int[4]{2,3,5,6}, 4},
+                                       {'*', new int[2]{3,5}, 2},
+                                       {'/', new int[5]{1,2,4,5,6}, 5},
+                                       {'|', new int[3]{4,5,6}, 3},
+                                       {'\'', new int[1]{4}, 1}
                                     };
 };
 
